a024fdab436ff5f63f922e7060bd664651eb66bc
MRL0R3 <ahmadrezajafary1384@gmail.com>
2025-04-12 20:08:04 +0330

    Updated

--- Java-Ring/src/main/java/org/project/entity/enemies/Enemy.java
+++ Java-Ring/src/main/java/org/project/entity/enemies/Enemy.java
@@ -1,35 +1,74 @@
 package org.project.entity.enemies;
 
+import org.project.entity.Entity;
 import org.project.object.weapons.Weapon;
 
-// TODO: UPDATE IMPLEMENTATION
-public abstract class Enemy {
-    Weapon weapon;
-    private int hp;
-    private int mp;
+public abstract class Enemy implements Entity {
 
-    public Enemy(int hp, int mp, Weapon weapon) {
-        this.hp = hp;
-        this.mp = mp;
+    protected final String name; 
+    protected int health;
+    protected int maxHealth;
+    protected Weapon weapon;
+    protected int expReward;
 
+    public Enemy(String name, int health, Weapon weapon, int expReward) {
+        this.name = name;
+        this.maxHealth = health;
+        this.health = health;
         this.weapon = weapon;
+        this.expReward = expReward;
     }
 
-    // TODO: (BONUS) UPDATE THE FORMULA OF TAKING DAMAGE
     @Override
-    public void takeDamage(int damage) {
-        hp -= damage;
+    public void attack(Entity target) {
+        System.out.println(name + " attacks!");
+        weapon.use(target);
     }
 
-    public int getHp() {
-        return hp;
+    @Override
+    public void takeDamage(int amount) {
+        health -= amount;
+        System.out.printf("%s took %d damage (%d/%d HP)%n",
+                name, amount, health, maxHealth);
     }
 
-    public int getMp() {
-        return mp;
+    @Override
+    public void heal(int amount) {
+        health = Math.min(health + amount, maxHealth);
+        System.out.println(name + " recovered " + amount + " HP!");
+    }
+    
+    @Override
+    public abstract String getDescription();
+    
+    // Getters
+    @Override
+    public String getName() {
+        return name;
     }
 
-    public Weapon getWeapon() {
-        return weapon;
+    @Override
+    public int getHealth() {
+        return health;
     }
+
+    @Override
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getExpReward() {
+        return expReward;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    @Override
+    public void defend() {
+        System.out.println(name + " braces for impact!");
+    }
+
 }
