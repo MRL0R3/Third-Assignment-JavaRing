8476a793e8e2cac43f7580298908df0e66c1a3ee
MRL0R3 <ahmadrezajafary1384@gmail.com>
2025-04-13 18:04:54 +0330

    Skeleton has two lives and a RustySword to attack

--- Java-Ring/src/main/java/org/project/entity/enemies/Skeleton.java
+++ Java-Ring/src/main/java/org/project/entity/enemies/Skeleton.java
@@ -1,15 +1,16 @@
 package org.project.entity.enemies;
 
+import org.project.entity.Entity;
+import org.project.object.weapons.RustySword;
 
-
-public class Skeleton extends Enemy implements Entity {
+public class Skeleton extends Enemy {
     private boolean hasResurrected;
-    
-    public Skeleton(int health, int expReward) {
+
+    public Skeleton(int health, int expReward, RustySword rustySword) {
         super("Skeleton", health, new RustySword(), expReward);
         this.hasResurrected = false;
     }
-    
+
     @Override
     public void takeDamage(int amount) {
         super.takeDamage(amount);
@@ -17,15 +18,32 @@ public class Skeleton extends Enemy implements Entity {
             resurrect();
         }
     }
-    
+
+    @Override
+    public String getDescription() {
+        return "A reanimated skeleton with a rusty sword. (Resurrects once)";
+    }
+
+    @Override
+    public String getName() {
+        return "Skeleton";
+    }
+
+    @Override
+    public void useSpecialAbility(Entity target) {
+        System.out.println("Sneaky Skeleton doesn't have a super power" +
+                " but this bastard got two lives ;) ");
+    }
+
+    @Override
+    public void gainExperience(int amount) {
+//        Enemies don't gain experience
+    }
+
     private void resurrect() {
         health = maxHealth / 2;
         hasResurrected = true;
         System.out.println("The skeleton reassembles itself!");
     }
 
-    // @Override
-    // Void string getDescription() {
-    //     return "A reanimated skeleton with a rusty sword. (Resurrects once)";
-    // }
 }
